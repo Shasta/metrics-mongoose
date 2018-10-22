8cccdba871745ca71373351ea2202f17896f9a2b
kartojal <canillas.mail@gmail.com>
2018-10-22 10:42:02 +0200

    Updated schema for single proof

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -236,22 +236,24 @@ var MetricProof = exports.MetricProof = new Schema({
     index: true,
     required: true
   },
-  metric_ipfs_hash: {
+  ipfs_hash: {
     type: String,
     index: true,
     required: true
   },
-  watts_consumed: {
-    type: Number,
-    required: true
-  },
-  watts_produced: {
-    type: Number,
-    required: true
-  },
-  metrics_timestamp: {
-    type: Date,
-    required: true
+  metrics: {
+    watts_consumed: {
+      type: Number,
+      required: true
+    },
+    watts_produced: {
+      type: Number,
+      required: true
+    },
+    timestamp: {
+      type: Date,
+      required: true
+    }
   }
 }, {
   // Add a timestamp when the doc is created and updated
